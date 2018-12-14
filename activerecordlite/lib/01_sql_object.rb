94015903ad72c688937988b044fc1b3076877c04
AndrewYW <andrewwang39@gmail.com>
2018-12-14 12:06:18 -0500

    finish finalize

--- activerecordlite/lib/01_sql_object.rb
+++ activerecordlite/lib/01_sql_object.rb
@@ -6,17 +6,44 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
+    return @columns if @columns
+    query = DBConnection.execute2(<<-SQL).first
+      SELECT * FROM cats LIMIT 0
+      SQL
+    
+    @columns = query.map(&:to_sym)
   end
 
   def self.finalize!
+    # columns.each do |name|
+    #   define_method(name) do 
+    #     self.instance_variable_get("@#{name}")
+    #   end
+    #   define_method("#{name}=") do |x|
+    #     self.instance_variable_set("@#{name}", x)
+    #   end
+    # end
+
+    self.columns.each do |name|
+      
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |x|
+        self.attributes[name] = x
+      end
+    end
   end
 
   def self.table_name=(table_name)
     # ...
+    @table_name = table_name
   end
 
   def self.table_name
     # ...
+    @table_name ? @table_name : "#{self.to_s.tableize}"
   end
 
   def self.all
@@ -37,6 +64,7 @@ class SQLObject
 
   def attributes
     # ...
+    @attributes ||= {}
   end
 
   def attribute_values
